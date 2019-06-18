e0cae8c117efbfbf01364239e16d940d8fc7c317
aryasasok <45318401+aryasasok@users.noreply.github.com>
2019-06-18 13:15:26 -0400

    first

---
+++ GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  ZombieConga
+//
+//  Created by Parrot on 2019-01-29.
+//  Copyright © 2019 Parrot. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+   
+    override func didMove(to view: SKView) {
+        // Set the background color of the app
+        self.backgroundColor = SKColor.black;
+    }
+    
+}
